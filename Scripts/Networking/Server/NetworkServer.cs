d90fe45efd471cbf36de025a504f9fd350567cf6
MOONBEAM\Owner <murthy1809@gmail.com>
2026-01-24 15:31:06 -0600

    client not spwanning

--- Scripts/Networking/Server/NetworkServer.cs
+++ Scripts/Networking/Server/NetworkServer.cs
@@ -24,16 +24,12 @@ public class NetworkServer : IDisposable
 
         networkManager.ConnectionApprovalCallback += ApprovalCheck;
         networkManager.OnServerStarted += OnNetworkReady;
-
-        Debug.Log("[NetworkServer] NetworkServer created, automatic spawning disabled");
     }
 
     private void ApprovalCheck(
         NetworkManager.ConnectionApprovalRequest request,
         NetworkManager.ConnectionApprovalResponse response)
     {
-        Debug.Log($"[NetworkServer] ApprovalCheck called for client {request.ClientNetworkId}");
-
         string payload = System.Text.Encoding.UTF8.GetString(request.Payload);
         UserData userData = JsonUtility.FromJson<UserData>(payload);
 
@@ -41,11 +37,8 @@ public class NetworkServer : IDisposable
         authIdToUserData[userData.userAuthId] = userData;
 
         response.Approved = true;
-        response.CreatePlayerObject = false; // We'll create it manually with correct position
+        response.CreatePlayerObject = false;
 
-        Debug.Log($"[NetworkServer] Approval granted. CreatePlayerObject = false. Will spawn manually.");
-
-        // Spawn player with custom position immediately
         if (response.Approved)
         {
             SpawnPlayerAtSpawnPoint(request.ClientNetworkId);
@@ -54,93 +47,53 @@ public class NetworkServer : IDisposable
 
     private void SpawnPlayerAtSpawnPoint(ulong clientId)
     {
-        Debug.Log($"[NetworkServer] SpawnPlayerAtSpawnPoint called for client {clientId}");
-
-        // Get spawn position from PlayerSpawnManager
         Vector3 spawnPosition = Vector3.zero;
         Quaternion spawnRotation = Quaternion.identity;
 
         if (PlayerSpawnManager.Instance != null)
         {
-            Debug.Log($"[NetworkServer] PlayerSpawnManager found!");
-            if (PlayerSpawnManager.Instance.TryGetSpawnPoint(clientId, out spawnPosition, out spawnRotation))
-            {
-                Debug.Log($"[NetworkServer] Got spawn point at {spawnPosition}");
-            }
-            else
-            {
-                Debug.LogWarning($"[NetworkServer] No spawn point available, using default");
-            }
+            PlayerSpawnManager.Instance.TryGetSpawnPoint(clientId, out spawnPosition, out spawnRotation);
         }
-        else
-        {
-            Debug.LogError("[NetworkServer] PlayerSpawnManager not found in scene!");
-        }
-
-        // Use our stored prefab reference instead of NetworkConfig.PlayerPrefab (which is now null)
-        Debug.Log($"[NetworkServer] Player prefab: {(playerPrefabReference != null ? playerPrefabReference.name : "NULL")}");
 
         if (playerPrefabReference != null)
         {
-            // Instantiate the player at the spawn point
             GameObject playerInstance = UnityEngine.Object.Instantiate(
                 playerPrefabReference,
                 spawnPosition,
                 spawnRotation
             );
 
-            Debug.Log($"[NetworkServer] Instantiated player '{playerInstance.name}' at {playerInstance.transform.position}");
-
             NetworkObject networkObject = playerInstance.GetComponent<NetworkObject>();
 
             if (networkObject != null)
             {
-                // Spawn as player object for this client
                 networkObject.SpawnAsPlayerObject(clientId, true);
-                Debug.Log($"[NetworkServer] ✓ SUCCESS: Spawned player for client {clientId} at {spawnPosition}");
             }
             else
             {
-                Debug.LogError("[NetworkServer] Player prefab missing NetworkObject component!");
                 UnityEngine.Object.Destroy(playerInstance);
             }
-        }
-        else
-        {
-            Debug.LogError("[NetworkServer] No player prefab reference stored!");
         }
     }
 
     private void OnNetworkReady()
     {
-        Debug.Log("[NetworkServer] OnNetworkReady called - Server started");
         networkManager.OnClientDisconnectCallback += OnClientDisconnect;
 
-        // Handle spawning the host player (clientId 0)
-        // The host doesn't go through ApprovalCheck, so we handle it here
         if (networkManager.IsHost)
         {
-            Debug.Log("[NetworkServer] Is Host - will spawn host player after scene loads");
             networkManager.SceneManager.OnLoadEventCompleted += OnSceneLoadCompleted;
         }
     }
 
     private void OnSceneLoadCompleted(string sceneName, UnityEngine.SceneManagement.LoadSceneMode loadSceneMode, System.Collections.Generic.List<ulong> clientsCompleted, System.Collections.Generic.List<ulong> clientsTimedOut)
     {
-        Debug.Log($"[NetworkServer] Scene '{sceneName}' loaded, spawning host player");
-
-        // Unsubscribe to avoid multiple calls
         networkManager.SceneManager.OnLoadEventCompleted -= OnSceneLoadCompleted;
-
-        // Spawn the host player at a spawn point
         SpawnPlayerAtSpawnPoint(networkManager.LocalClientId);
     }
 
     private void OnClientDisconnect(ulong clientId)
     {
-        Debug.Log($"[NetworkServer] Client {clientId} disconnected");
-
-        // Release the spawn point so it can be reused
         if (PlayerSpawnManager.Instance != null)
         {
             PlayerSpawnManager.Instance.ReleaseSpawnPoint(clientId);
