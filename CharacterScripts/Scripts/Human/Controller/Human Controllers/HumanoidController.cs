43cae51b5f8b7dcae823e2fd769d70a32e6dc426
MOONBEAM\Owner <murthy1809@gmail.com>
2026-02-28 18:19:45 -0600

    added player UI for testing, fixed jumpheigh issue, need to test with client

--- CharacterScripts/Scripts/Human/Controller/Human Controllers/HumanoidController.cs
+++ CharacterScripts/Scripts/Human/Controller/Human Controllers/HumanoidController.cs
@@ -128,7 +128,9 @@ public class HumanoidController : ThirdPersonController
             speed = combatSpeed;
             jumpHeight = 0;
         }
-          
+        else if (!playerController.inputController.isCombatMode)
+            jumpHeight = 2;
+
         else if (playerController.inputController.isCrouch)
             speed = crouchSpeed;
         else
