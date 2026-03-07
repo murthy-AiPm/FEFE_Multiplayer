c22d1e312046df01ff7471ee362b2f1c60fa17f7
MOONBEAM\Owner <murthy1809@gmail.com>
2026-03-07 10:28:01 -0600

    Built V0.6 and made todo list

---
+++ ToDo.cs
@@ -0,0 +1,16 @@
+/* 1. Setup horse height
+ * 2. Setup Arrow spwan point
+ * 3. Zoom out camera bit or add camera controls
+ * 4. Add higher resolutions
+ * 5. Disable controls on pause menu
+ * 6. Hide player UI on pause menu
+ * 7. Setup Spawn points
+ * 8. Setup Death screen with spawn points
+ * 9. Setup loading screens to hide camera switching
+ * 10. Add mouse sensitivity for Player, bow, and horse
+ * 11. Tweak combat rotation speed
+ * 12. Expose mouse sensitivity on settings
+ * 13.Expose key bindings on settings
+ * 14. Single space dodgestep, double space dodge
+ * 15. Horse freeze all when idle */
+
